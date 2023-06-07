f7d8f82c984b05fdb47c11559e3e576d1057009c
beboo22 <mt557829@gmail.com>
2023-06-07 14:51:55 +0300

    taske

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0,10):
-        if i + j == 18:
-            print("{:d}{:d}".format(i,j))
-        else:
-            print("{:d}{:d} ,".format(i,j),end='')
+for i in range(0, 100):
+    if i < 10 and i > 0:
+        print("0{}, ".format(i),end='')
+    elif i  == 99:
+        print("{:d}".format(i))
+    elif i >= 10:
+        print("{:d}, ".format(i),end='')
