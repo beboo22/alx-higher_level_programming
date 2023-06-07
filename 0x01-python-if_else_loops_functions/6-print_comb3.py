e58b32bcafe9fa47b3de5da32231bcdc26456248
beboo22 <mt557829@gmail.com>
2023-06-07 15:12:20 +0300

    taske

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(1, 10):
+    for j in range(0, 10):
         if i >= j:
             continue
-        elif i == 8 and j == 9:
-            print("{}{}".format(i, j))
+        elif i + j == 17:
+            print("{:d}{:d}".format(i, j))
         else:
-            print("{}{}, ".format(i, j), end="")
+            print("{:d}{:d}, ".format(i, j), end='')
