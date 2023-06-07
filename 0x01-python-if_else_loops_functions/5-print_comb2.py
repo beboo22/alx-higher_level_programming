e76d5bd9078dbb44d70d57fc3af5a24b65bd6e90
beboo22 <mt557829@gmail.com>
2023-06-07 14:54:06 +0300

    taske

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10 and i > 0:
-        print("0{}, ".format(i),end='')
-    elif i  == 99:
-        print("{:d}".format(i))
-    elif i >= 10:
-        print("{:d}, ".format(i),end='')
+    if i == 99:
+        print("99")
+    else:
+        print("{0:0=2d}, ".format(i), end='')
