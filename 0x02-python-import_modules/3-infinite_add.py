66dd769e6f80ec3851d080d28bba156d98f7df5d
beboo22 <mt557829@gmail.com>
2023-06-08 16:43:05 +0300

    task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv)
+    y = 0
+    for i in range(1, n):
+        y += int(argv[i])
+    print(y)
