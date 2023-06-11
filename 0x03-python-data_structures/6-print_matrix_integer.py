78ec37d3f19bd05109380d1ae60fd7ac7e26ff58
beboo22 <mt557829@gmail.com>
2023-06-11 15:41:25 +0300

    task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     if matrix != [[]]:
         for row in matrix:
             for ele in row:
-                print("{:d}".format(ele), end="" if ele != row[-1] else "\n")
+                print("{:d}".format(ele), end=" " if ele != row[-1] else "\n")
     else:
         print("")
