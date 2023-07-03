8975289c3ede25caf53dac6a58a350907224cb18
beboo22 <mt557829@gmail.com>
2023-07-04 02:02:21 +0300

    task

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,11 +50,13 @@ class Rectangle:
         return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        w = ''
+        """presents a diagram of the rectangle defined for an object"""
         if self.__width == 0 or self.__height == 0:
-            return ''
-        else:
-            for i in range(self.height):
-                w += '#' * self.width
-                w += '\n'
-            return w
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
