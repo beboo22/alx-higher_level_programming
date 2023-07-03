6173c5182e41892d34c5568ed015d20bd5badc71
beboo22 <mt557829@gmail.com>
2023-07-04 01:14:55 +0300

    task

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+Defines a class Rectangle
+"""
+
+
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
