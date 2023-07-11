b0f05f6136c4dc586cde727245049257eb38b7a1
beboo22 <mt557829@gmail.com>
2023-07-12 02:12:51 +0300

    task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,7 +12,7 @@ def read_file(filename=""):
         Exception: when the file can be opened
 
     """
-    
+
     with open(filename, 'r', encoding="UTF8") as f:
-        r_data = read(filename)
+        r_data = f.read(filename)
         print(filename, end='')
