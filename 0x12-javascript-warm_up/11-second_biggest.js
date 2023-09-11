9d836d95f6145bbe3269da2e2cb834419a5d5dca
beboo22 <mt557829@gmail.com>
2023-09-11 19:34:42 +0300

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
+// searches the second biggest integer in the list of arguments.
 
-if (process.argv.length < 4) {
-  console.log('0');
-} else {
-  const size = process.argv.length;
-  const ints = [];
-  for (let i = 2; i < size; i++) {
-    ints[i - 2] = parseInt(process.argv[i]);
+if (process.argv.length <= 3) {
+    console.log(0);
+  } else {
+    const list = process.argv.sort();
+    console.log(list.reverse()[1]);
   }
-  ints.sort(function (a, b) { return b - a; });
-  console.log(ints[1]);
-}
