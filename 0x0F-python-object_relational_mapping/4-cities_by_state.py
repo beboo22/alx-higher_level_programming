dc5939c5e32ab9ba25cf36459194c3dc411d0bbe
beboo22 <mt557829@gmail.com>
2023-09-14 19:18:09 +0300

    task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the database 'hbtn_0e_0_usa'.
+Usage: ./script.py <username> <password> <database>
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    username, password, database = argv[1], argv[2], argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    INNER JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC
+    """
+
+    cur = db.cursor()
+    cur.execute(query)
+    res = cur.fetchall()
+
+    for row in res:
+        print(row)
