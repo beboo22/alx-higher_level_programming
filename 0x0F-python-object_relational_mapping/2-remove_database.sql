bf72a5239b5162cf6f2c9cc9298f510ceec4970f
beboo22 <mt557829@gmail.com>
2023-09-15 14:06:25 +0300

    task

--- 0x0F-python-object_relational_mapping/2-remove_database.sql
+++ 0x0F-python-object_relational_mapping/2-remove_database.sql
@@ -2,4 +2,4 @@
 -- If the database hbtn_0c_0 doesn't exist, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 
-DROP DATABASE IF EXISTS hbtn_0e_0_usa;
+DROP DATABASE IF EXISTS hbtn_0e_6_usa;
