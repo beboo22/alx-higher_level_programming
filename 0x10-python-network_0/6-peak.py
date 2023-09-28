d0f0f3fc11cdb97013058c1fed88f4fc98997283
beboo22 <mt557829@gmail.com>
2023-09-28 23:42:26 +0300

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
 def find_peak(list_of_integers):
-   x = len(list_of_integers)
-   if (x == 0):
-    return None
-   if (list_of_integers[0] > list_of_integers[1]):
-    return list_of_integers[0]
+    x = len(list_of_integers)
+    if x == 0:
+        return None
 
-   if (list_of_integers[x] > list_of_integers[x-1]):
-    return list_of_integers[x]
+    if x == 1:
+        return list_of_integers[0]
 
-   for i in range(x):
+    if list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
 
+    if list_of_integers[x-1] > list_of_integers[x-2]:
+        return list_of_integers[x-1]
+
+    for i in range(1, x-1):
+        if list_of_integers[i] > list_of_integers[i-1] and list_of_integers[i] > list_of_integers[i+1]:
+            return list_of_integers[i]
+        elif list_of_integers[i-1] == list_of_integers[i] and list_of_integers[i] == list_of_integers[i+1]:
+            return list_of_integers[i]
