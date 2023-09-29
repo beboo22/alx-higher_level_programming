e7655da211b9931bdd4c3e968562e943195f7f37
beboo22 <mt557829@gmail.com>
2023-09-30 01:01:01 +0300

    task

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL & displays the body of the response
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    req = request.Request(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
