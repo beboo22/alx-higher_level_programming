b5dee43585980d77ddb39b050ed7903baab1ba3f
beboo22 <mt557829@gmail.com>
2023-09-30 00:52:47 +0300

    task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the value of the
-X-Request-Id variable found in the header of the response
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
 """
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     from sys import argv
-    v = {'email': argv[2]}
-    req = requests.post(argv[1],parmeter=v)
-    print(req.text)
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
