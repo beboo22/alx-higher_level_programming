08f391794cd7f6f45f682cdb0a78ea0d920010e9
beboo22 <mt557829@gmail.com>
2023-09-30 01:02:28 +0300

    task

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,11 +2,12 @@
 """
 takes in a URL, sends a request to the URL & displays the body of the response
 """
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     from sys import argv
-    req = request.Request(argv[1])
-    if req.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(req.text)
+        print(r.text)
+        
